be4e2092750c02681961d6c4da66191b8b460af8
Salosion <denis-stepurka@mail.ru>
2022-07-21 17:25:45 +0600

    added adaptive 1000px

--- app/js/main.js
+++ app/js/main.js
@@ -5,8 +5,17 @@ $(function () {
     centerPadding: '125px',
     slidesToShow: 2.3,
     slidesToScroll: 1,
+    adaptiveHeight: true,
     nextArrow: '<img class="next-arrow" src="images/icons/next-arrow.svg">',
     prevArrow: '<img class="prev-arrow" src="images/icons/prev-arrow.svg">',
+  //   responsive: [
+  //     {
+  //       breakpoint: 1000,
+  //       settings: {
+  //         centerPadding: '10px',
+  //       }
+  //     }
+  // ]
   });
 
 });
