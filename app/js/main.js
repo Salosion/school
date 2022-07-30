06711704542158bead8432cd9c99b4095016b454
Salosion <denis-stepurka@mail.ru>
2022-07-30 15:34:20 +0600

    added adaptive menu, fixed header

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,30 @@
 $(function () {
 
+  $('.menu__btn, .menu a').on('click', function () {
+    $('.header__box').toggleClass('header__box--active');
+  });
+
+  $(".menu").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+
   $('.reviews__items').slick({
-    centerMode: true,
-    centerPadding: '125px',
-    slidesToShow: 2.3,
+    slidesToShow: 2.9,
+    infinite: true,
     slidesToScroll: 1,
-    adaptiveHeight: true,
     nextArrow: '<img class="next-arrow" src="images/icons/next-arrow.svg">',
     prevArrow: '<img class="prev-arrow" src="images/icons/prev-arrow.svg">',
-  //   responsive: [
-  //     {
-  //       breakpoint: 1000,
-  //       settings: {
-  //         centerPadding: '10px',
-  //       }
-  //     }
-  // ]
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2.5,
+        }
+      }
+    ]
   });
 
 });
